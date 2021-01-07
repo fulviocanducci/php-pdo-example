f210d6054d1a1e35045af5fe380037d61f2e88a7
fulviocanducci <fulviocanducci@hotmail.com>
2021-01-07 20:29:44 -0300

    Methods Magic

--- DTO/people.php
+++ DTO/people.php
@@ -4,21 +4,35 @@ class People  {
   private $id;
   private $name;
 
-  public function setId(int $id): People {
+  public function setId(int $id): People 
+  {
     $this->id = $id;
     return $this;
   }
 
-  public function setName(string $name): People {
+  public function setName(string $name): People 
+  {
     $this->name = $name;
     return $this;
   }
 
-  public function getId(): int {
+  public function getId(): int 
+  {
     return $this->id;
   }
 
-  public function getName(): string {
+  public function getName(): string 
+  {
     return $this->name;
+  }
+
+  public function __get($name) 
+  {
+    return $this->$name;
+  }
+
+  public function __set($name, $value) 
+  {
+    return $this->$name = $value;
   }
 }
--- index.php
+++ index.php
@@ -8,15 +8,21 @@ $connect = new Connect();
 
 $daoPeople = new DaoPeople($connect);
 
+$people = $daoPeople->get(1);
+
+echo $people->id . ' - ' . $people->name;
+
+
 /*
 $people = new People();
 $people->setName('Ministro Tarcisio de Freitas');
 
 var_dump($daoPeople->insert($people));
-*/
+
 
 $people = new People();
 $people->setId(2);
 $people->setName('Ministro da Saúde Pauzelo');
 
 print_r($daoPeople->update($people));
+*/
